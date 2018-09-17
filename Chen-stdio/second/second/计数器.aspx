54bda85f54fe7fb10a6d6882cf5c979c028c176d
一心 <ulquiola@163.com>
2018-09-17 10:42:11 +0800

    second

---
+++ Chen-stdio/second/second/计数器.aspx
@@ -0,0 +1,21 @@
+﻿<%@ Page Language="C#" AutoEventWireup="true" CodeBehind="计数器.aspx.cs" Inherits="second.计数器" %>
+
+<!DOCTYPE html>
+
+<html xmlns="http://www.w3.org/1999/xhtml">
+<head runat="server">
+<meta http-equiv="Content-Type" content="text/html; charset=utf-8"/>
+    <title></title>
+</head>
+<body>
+    <form id="form1" runat="server">
+    <div>
+    
+    </div>
+        <asp:Button ID="Button1" runat="server" Text="单击我" OnClick="Button1_Click" /><br /><br />
+        你一共点击了<asp:TextBox ID="cishu" runat="server" Width="46px"></asp:TextBox>
+&nbsp; 次<br /><br />
+
+    </form>
+</body>
+</html>
---
+++ Chen-stdio/second/second/计数器.aspx.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+
+namespace second
+{
+    public partial class 计数器 : System.Web.UI.Page
+    {
+        protected void Page_Load(object sender, EventArgs e)
+        {
+            if(!IsPostBack)
+            {
+                ViewState ["counter"]=0;
+            }
+
+        }
+
+        protected void Button1_Click(object sender, EventArgs e)
+        {
+            int counter =Convert.ToInt32( ViewState["counter"]);
+            counter++;
+            ViewState["counter"] = counter;
+            cishu.Text = counter.ToString();
+            //Response.Write(cishu.Text);
+
+        }
+    }
+}
