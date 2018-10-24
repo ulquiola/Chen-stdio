8a5fbc3030e3c964868a619743a035482d934adc
一心 <ulquiola@163.com>
2018-10-24 11:41:25 +0800

    1

---
+++ Chen-stdio/BookStore/BookStore/Controllers/BookStoreController.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace BookStore.Controllers
+{
+    public class BookStoreController : Controller
+    {
+        Models.bookMVCEntities db = new Models.bookMVCEntities();        
+        // GET: BookStore
+        public ActionResult Index()
+        {
+            var data = db.Books;
+            return View(data);
+        }
+    }
+}
